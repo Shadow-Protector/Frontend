8d9e51790dbaa4b5dc85e3c1aea9a97a6b7544c0
parizval <anmolgoyal@gmail.com>
2025-05-15 00:31:36 +0530

    created initial version of condition Component

--- src/app/create/components/condition/ConditionSection.tsx
+++ src/app/create/components/condition/ConditionSection.tsx
@@ -49,6 +49,13 @@ export function ConditionSection({
             }
             updateConditionObjectWithInput={updateConditionObjectWithInput}
           />
+          <TipComponent
+            conditionObject={conditionObject}
+            updateConditionObjectWithSelector={
+              updateConditionObjectWithSelector
+            }
+            updateConditionObjectWithInput={updateConditionObjectWithInput}
+          />
         </div>
       </div>
     </>
@@ -207,4 +214,45 @@ function TipComponent({
   conditionObject,
   updateConditionObjectWithInput,
   updateConditionObjectWithSelector,
-}: ConditionFormProps) {}
+}: ConditionFormProps) {
+  return (
+    <>
+      Select Tip Token Address: {conditionObject.tipTokenAddress}
+      <select
+        name="tipTokenAddress"
+        className="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+        onChange={updateConditionObjectWithSelector}
+      >
+        <option value={""}>Select Token</option>
+        <option value={"asda"}>USDC</option>
+      </select>
+      <br />
+      <div className="max-w-sm space-y-3">
+        <div>
+          <label
+            htmlFor="tipTokenAmount"
+            className="block text-sm font-medium mb-2 dark:text-white"
+          >
+            Enter Value: {conditionObject.tipTokenAmount}
+          </label>
+          <div className="relative">
+            <input
+              type="text"
+              id="hs-input-with-leading-and-trailing-icon"
+              name="tipTokenAmount"
+              className="py-2.5 sm:py-3 px-4 ps-9 pe-16 block w-full border-gray-200 rounded-lg sm:text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+              placeholder="0.00"
+              onChange={updateConditionObjectWithInput}
+            />
+            <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
+              <span className="text-gray-500 dark:text-neutral-500">$</span>
+            </div>
+            <div className="absolute inset-y-0 end-0 flex items-center pointer-events-none z-20 pe-4">
+              <span className="text-gray-500 dark:text-neutral-500">USD</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
