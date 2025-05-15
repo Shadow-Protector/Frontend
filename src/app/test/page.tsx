78f8b493b96b9024902935a13af3942ca109357b
parizval <anmolgoyal@gmail.com>
2025-05-16 00:50:37 +0530

    removed api module and test page for deployment

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,92 +1,92 @@
-"use client";
-// import { useEvmNativeBalance } from "@moralisweb3/next";
+// "use client";
+// // import { useEvmNativeBalance } from "@moralisweb3/next";
 
-import { useState } from "react";
-import {
-  Label,
-  Listbox,
-  ListboxButton,
-  ListboxOption,
-  ListboxOptions,
-} from "@headlessui/react";
-import { ChevronUpDownIcon } from "@heroicons/react/16/solid";
-import { CheckIcon } from "@heroicons/react/20/solid";
+// import { useState } from "react";
+// import {
+//   Label,
+//   Listbox,
+//   ListboxButton,
+//   ListboxOption,
+//   ListboxOptions,
+// } from "@headlessui/react";
+// import { ChevronUpDownIcon } from "@heroicons/react/16/solid";
+// import { CheckIcon } from "@heroicons/react/20/solid";
 
-const people = [
-  {
-    id: 1,
-    name: "Wade Cooper",
-    avatar: "/base.jpeg",
-  },
-  {
-    id: 2,
-    name: "Arlene Mccoy",
-    avatar: "arbitrum.jpeg",
-  },
-  {
-    id: 3,
-    name: "Devon Webb",
-    avatar:
-      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80",
-  },
-];
+// const people = [
+//   {
+//     id: 1,
+//     name: "Wade Cooper",
+//     avatar: "/base.jpeg",
+//   },
+//   {
+//     id: 2,
+//     name: "Arlene Mccoy",
+//     avatar: "arbitrum.jpeg",
+//   },
+//   {
+//     id: 3,
+//     name: "Devon Webb",
+//     avatar:
+//       "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80",
+//   },
+// ];
 
-export default function Testing() {
-  const [selected, setSelected] = useState(people[3]);
+// export default function Testing() {
+//   const [selected, setSelected] = useState(people[3]);
 
-  return (
-    <>
-      <div>
-        <Listbox value={selected} onChange={setSelected}>
-          <Label className="block text-sm/6 font-medium text-gray-900">
-            Assigned to {selected.name}
-          </Label>
-          <div className="relative mt-2">
-            <ListboxButton className="grid w-full cursor-default grid-cols-1 rounded-md bg-white py-1.5 pr-2 pl-3 text-left text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-              <span className="col-start-1 row-start-1 flex items-center gap-3 pr-6">
-                <img
-                  alt=""
-                  src={selected.avatar}
-                  className="size-5 shrink-0 rounded-full"
-                />
-                <span className="block truncate">{selected.name}</span>
-              </span>
-              <ChevronUpDownIcon
-                aria-hidden="true"
-                className="col-start-1 row-start-1 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-              />
-            </ListboxButton>
+//   return (
+//     <>
+//       <div>
+//         <Listbox value={selected} onChange={setSelected}>
+//           <Label className="block text-sm/6 font-medium text-gray-900">
+//             Assigned to {selected.name}
+//           </Label>
+//           <div className="relative mt-2">
+//             <ListboxButton className="grid w-full cursor-default grid-cols-1 rounded-md bg-white py-1.5 pr-2 pl-3 text-left text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+//               <span className="col-start-1 row-start-1 flex items-center gap-3 pr-6">
+//                 <img
+//                   alt=""
+//                   src={selected.avatar}
+//                   className="size-5 shrink-0 rounded-full"
+//                 />
+//                 <span className="block truncate">{selected.name}</span>
+//               </span>
+//               <ChevronUpDownIcon
+//                 aria-hidden="true"
+//                 className="col-start-1 row-start-1 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+//               />
+//             </ListboxButton>
 
-            <ListboxOptions
-              transition
-              className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-hidden data-leave:transition data-leave:duration-100 data-leave:ease-in data-closed:data-leave:opacity-0 sm:text-sm"
-            >
-              {people.map((person) => (
-                <ListboxOption
-                  key={person.id}
-                  value={person}
-                  className="group relative cursor-default py-2 pr-9 pl-3 text-gray-900 select-none data-focus:bg-indigo-600 data-focus:text-white data-focus:outline-hidden"
-                >
-                  <div className="flex items-center">
-                    <img
-                      alt=""
-                      src={person.avatar}
-                      className="size-5 shrink-0 rounded-full"
-                    />
-                    <span className="ml-3 block truncate font-normal group-data-selected:font-semibold">
-                      {person.name}
-                    </span>
-                  </div>
+//             <ListboxOptions
+//               transition
+//               className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-hidden data-leave:transition data-leave:duration-100 data-leave:ease-in data-closed:data-leave:opacity-0 sm:text-sm"
+//             >
+//               {people.map((person) => (
+//                 <ListboxOption
+//                   key={person.id}
+//                   value={person}
+//                   className="group relative cursor-default py-2 pr-9 pl-3 text-gray-900 select-none data-focus:bg-indigo-600 data-focus:text-white data-focus:outline-hidden"
+//                 >
+//                   <div className="flex items-center">
+//                     <img
+//                       alt=""
+//                       src={person.avatar}
+//                       className="size-5 shrink-0 rounded-full"
+//                     />
+//                     <span className="ml-3 block truncate font-normal group-data-selected:font-semibold">
+//                       {person.name}
+//                     </span>
+//                   </div>
 
-                  <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-not-data-selected:hidden group-data-focus:text-white">
-                    <CheckIcon aria-hidden="true" className="size-5" />
-                  </span>
-                </ListboxOption>
-              ))}
-            </ListboxOptions>
-          </div>
-        </Listbox>
-      </div>
-    </>
-  );
-}
+//                   <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-not-data-selected:hidden group-data-focus:text-white">
+//                     <CheckIcon aria-hidden="true" className="size-5" />
+//                   </span>
+//                 </ListboxOption>
+//               ))}
+//             </ListboxOptions>
+//           </div>
+//         </Listbox>
+//       </div>
+//     </>
+//   );
+// }
