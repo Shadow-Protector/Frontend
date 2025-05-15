04d6f38f960a4affd8275770638ce0ea7b91b5f3
parizval <anmolgoyal@gmail.com>
2025-05-16 00:40:15 +0530

    commented linting error part for deployment

--- src/app/create/components/condition/ConditionSection.tsx
+++ src/app/create/components/condition/ConditionSection.tsx
@@ -251,137 +251,137 @@ function ConditionPlatformAddress({
   } else if (conditionObject.platform == 4) {
   }
 
-  async function updateConditionPlatformAddress(
-    e: ChangeEvent<HTMLSelectElement>,
-  ) {
-    const { name, value } = e.target;
-    console.log(name, value);
-    updateConditionObject("platformAddress", value);
-  }
+  // async function updateConditionPlatformAddress(
+  //   e: ChangeEvent<HTMLSelectElement>,
+  // ) {
+  //   const { name, value } = e.target;
+  //   console.log(name, value);
+  //   updateConditionObject("platformAddress", value);
+  // }
 
-  console.log("Condition Platform", conditionObject.platform);
-  if (conditionObject.platform >= 0) {
-    const array =
-      ConditionData[conditionObject.chainId.toString()].platformAddress[
-        conditionObject.platform.toString()
-      ];
+  // console.log("Condition Platform", conditionObject.platform);
+  // if (conditionObject.platform >= 0) {
+  //   const array =
+  //     ConditionData[conditionObject.chainId.toString()].platformAddress[
+  //       conditionObject.platform.toString()
+  //     ];
 
-    if (array == undefined) {
-      return;
-    }
+  //   if (array == undefined) {
+  //     return;
+  //   }
 
-    const listItem = array.map((object) => (
-      <option key={object.id} value={object.value}>
-        {object.name}
-      </option>
-    ));
-    // console.log("Address Call", listItem);
+  //   const listItem = array.map((object) => (
+  //     <option key={object.id} value={object.value}>
+  //       {object.name}
+  //     </option>
+  //   ));
+  //   // console.log("Address Call", listItem);
 
-    return (
-      <>
-        Select Condition Platform Address:
-        {`${conditionObject.platformAddress?.slice(0, 4)}..${conditionObject.platformAddress?.slice(-4)}`}
-        <select
-          name="platformAddress"
-          className="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-          onChange={updateConditionPlatformAddress}
-        >
-          <option>Select Platform</option>
-          {listItem}
-        </select>
-        <br />
-      </>
-    );
-  }
+  //   return (
+  //     <>
+  //       Select Condition Platform Address:
+  //       {`${conditionObject.platformAddress?.slice(0, 4)}..${conditionObject.platformAddress?.slice(-4)}`}
+  //       <select
+  //         name="platformAddress"
+  //         className="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+  //         onChange={updateConditionPlatformAddress}
+  //       >
+  //         <option>Select Platform</option>
+  //         {listItem}
+  //       </select>
+  //       <br />
+  //     </>
+  //   );
+  // }
 
   return;
 }
 
-function ConditionParamterType({
-  conditionObject,
-  updateConditionObject,
-}: ConditionProps) {
-  async function updateConditionParameter(e: ChangeEvent<HTMLSelectElement>) {
-    const { name, value } = e.target;
-    console.log(name, value);
+// function ConditionParamterType({
+//   conditionObject,
+//   updateConditionObject,
+// }: ConditionProps) {
+//   async function updateConditionParameter(e: ChangeEvent<HTMLSelectElement>) {
+//     const { name, value } = e.target;
+//     console.log(name, value);
 
-    updateConditionObject("parameter", value);
-  }
+//     updateConditionObject("parameter", value);
+//   }
 
-  async function updateConditionValue(e: ChangeEvent<HTMLInputElement>) {
-    const { name, value } = e.target;
-    console.log(name, value);
+//   async function updateConditionValue(e: ChangeEvent<HTMLInputElement>) {
+//     const { name, value } = e.target;
+//     console.log(name, value);
 
-    updateConditionObject("conditionValue", value);
-  }
+//     updateConditionObject("conditionValue", value);
+//   }
 
-  if (conditionObject.platform >= 0 && conditionObject.platformAddress != "") {
-    console.log(
-      "",
-      ConditionData[conditionObject.chainId].parameterType[
-        conditionObject.platform
-      ],
-    );
+//   if (conditionObject.platform >= 0 && conditionObject.platformAddress != "") {
+//     console.log(
+//       "",
+//       ConditionData[conditionObject.chainId].parameterType[
+//         conditionObject.platform
+//       ],
+//     );
 
-    const array =
-      ConditionData[conditionObject.chainId].parameterType[
-        conditionObject.platform
-      ];
+//     const array =
+//       ConditionData[conditionObject.chainId].parameterType[
+//         conditionObject.platform
+//       ];
 
-    if (array == undefined) {
-      return;
-    }
+//     if (array == undefined) {
+//       return;
+//     }
 
-    const listItem = array.map((object: { id: number; name: string }) => (
-      <option key={object.id} value={object.id}>
-        {object.name}
-      </option>
-    ));
+//     const listItem = array.map((object: { id: number; name: string }) => (
+//       <option key={object.id} value={object.id}>
+//         {object.name}
+//       </option>
+//     ));
 
-    return (
-      <>
-        Select Condition:
-        {conditionObject.parameter >= 0 ? conditionObject.parameter : ""}
-        <select
-          name="parameter"
-          className="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-          onChange={updateConditionParameter}
-        >
-          <option value={-1}>Select Condition</option>
-          {listItem}
-        </select>
-        <br />
-        <div className="max-w-sm space-y-3">
-          <div>
-            <label
-              htmlFor="conditionValue"
-              className="block text-sm font-medium mb-2 dark:text-white"
-            >
-              Enter Value: {conditionObject.conditionValue}
-            </label>
-            <div className="relative">
-              <input
-                type="text"
-                id="hs-input-with-leading-and-trailing-icon"
-                name="conditionValue"
-                className="py-2.5 sm:py-3 px-4 ps-9 pe-16 block w-full border-gray-200 rounded-lg sm:text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-                placeholder="0.00"
-                onChange={updateConditionValue}
-              />
-              <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
-                <span className="text-gray-500 dark:text-neutral-500">$</span>
-              </div>
-              <div className="absolute inset-y-0 end-0 flex items-center pointer-events-none z-20 pe-4">
-                <span className="text-gray-500 dark:text-neutral-500">USD</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  }
-  return;
-}
+//     return (
+//       <>
+//         Select Condition:
+//         {conditionObject.parameter >= 0 ? conditionObject.parameter : ""}
+//         <select
+//           name="parameter"
+//           className="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+//           onChange={updateConditionParameter}
+//         >
+//           <option value={-1}>Select Condition</option>
+//           {listItem}
+//         </select>
+//         <br />
+//         <div className="max-w-sm space-y-3">
+//           <div>
+//             <label
+//               htmlFor="conditionValue"
+//               className="block text-sm font-medium mb-2 dark:text-white"
+//             >
+//               Enter Value: {conditionObject.conditionValue}
+//             </label>
+//             <div className="relative">
+//               <input
+//                 type="text"
+//                 id="hs-input-with-leading-and-trailing-icon"
+//                 name="conditionValue"
+//                 className="py-2.5 sm:py-3 px-4 ps-9 pe-16 block w-full border-gray-200 rounded-lg sm:text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+//                 placeholder="0.00"
+//                 onChange={updateConditionValue}
+//               />
+//               <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
+//                 <span className="text-gray-500 dark:text-neutral-500">$</span>
+//               </div>
+//               <div className="absolute inset-y-0 end-0 flex items-center pointer-events-none z-20 pe-4">
+//                 <span className="text-gray-500 dark:text-neutral-500">USD</span>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </>
+//     );
+//   }
+//   return;
+// }
 
 function TipComponent({
   conditionObject,
